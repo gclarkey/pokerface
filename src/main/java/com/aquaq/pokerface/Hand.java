45b9c2a1d35938592cf91f7408960fe364e266ad
gclarkey <gareth.clarke@aquaq.co.uk>
2017-09-14 09:19:22 +0100

    Created Hand object and pushed some responsibility into it.

---
+++ src/main/java/com/aquaq/pokerface/Hand.java
@@ -0,0 +1,65 @@
+package com.aquaq.pokerface;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Hand {
+
+    private static final int MAX_HAND_SIZE = 5;
+    private Set<Card> cards = new HashSet<>();
+
+    public void addCard(final Card card){
+        if(cards.size() == MAX_HAND_SIZE){
+            final String message = String.format("Number of cards: %s in hand: %s exceeds limit: %s. Cannot add card: %s", cards.size(), cards, MAX_HAND_SIZE, card);
+            System.out.println(message);
+            throw new PokerFaceException(message);
+        }
+        if(!cards.add(card)){
+            final String message = String.format("Attempt to add duplicate card: %s to hand: %s. Input data invalid.", card, cards);
+            System.out.println(message);
+            throw new PokerFaceException(message);
+        }
+    }
+
+    public String getHandRank(){
+        final String description;
+
+        if(straightFlush(cards)){
+            description = "Straight flush";
+        } else if(fourOfAKind(cards)){
+            description = "Four of a kind";
+        } else if(fullHouse(cards)){
+            description = "Full house";
+        } else if(twoPair(cards)){
+            description = "Two pair";
+        } else if(onePair(cards)){
+            description = "One pair";
+        } else {
+            final String message = String.format("Unable to determine description for cards: %s", cards);
+            System.out.println(message);
+            throw new PokerFaceException(message);
+        }
+
+        return description;
+    }
+
+    private boolean straightFlush(Set<Card> hand) {
+        return false;
+    }
+
+    private boolean fourOfAKind(Set<Card> hand) {
+        return false;
+    }
+
+    private boolean fullHouse(Set<Card> hand) {
+        return false;
+    }
+
+    private boolean twoPair(Set<Card> hand) {
+        return false;
+    }
+
+    private boolean onePair(Set<Card> hand) {
+        return true;
+    }
+}
--- src/main/java/com/aquaq/pokerface/PokerFace.java
+++ src/main/java/com/aquaq/pokerface/PokerFace.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,7 +15,6 @@ public class PokerFace {
     private static final int POSITION_SUIT = 1;
     private static final int POSITION_NAME = 0;
     private static final int EXPECTED_CARD_DATA_LENGTH = 2;
-    private static final int EXPECTED_CARD_LIST_SIZE = 5;
 
     public void readInput(){
         Stream<String> input = null;
@@ -32,79 +30,24 @@ public class PokerFace {
             throw new PokerFaceException(message);
         }
         //input.forEach(System.out::println);
-        List<String> record = input.collect(Collectors.toList());
-        for(String s : record){
-            System.out.println(s);
-            System.out.println(determineHand(s));
+        final List<String> record = input.collect(Collectors.toList());
+        for(final String hand : record){
+            final String description = determineHand(hand);
+            System.out.println(String.format("Hand: %s, Description: %s.", hand, description));
         }
     }
 
-    public String determineHand(final String cards){
-        final List<Card> hand = new ArrayList<>();
+    private String determineHand(final String cards){
+        final Hand hand = new Hand();
         final List<String> cardList = Arrays.asList(cards.split(DELIMITER));
-        validateCardList(cardList);
         for(final String card : cardList){
             validateCard(card);
             char suit = card.charAt(POSITION_SUIT);
-            //validateSuit(suit);
             char name = card.charAt(POSITION_NAME);
-            //validateName(name);
-            hand.add(new Card(suit, name));
-        }
-        final String handDescription = determineHandDescription(hand);
-
-        return handDescription;
-    }
-
-    private String determineHandDescription(final List<Card> hand) {
-        final String description;
-
-        if(straightFlush(hand)){
-            description = "Straight flush";
-        } else if(fourOfAKind(hand)){
-            description = "Four of a kind";
-        } else if(fullHouse(hand)){
-            description = "Full house";
-        } else if(twoPair(hand)){
-            description = "Two pair";
-        } else if(onePair(hand)){
-            description = "One pair";
-        } else {
-            final String message = String.format("Unable to determine description for hand: %s", hand);
-            System.out.println(message);
-            throw new PokerFaceException(message);
+            hand.addCard(new Card(suit, name));
         }
 
-        return description;
-    }
-
-    private boolean straightFlush(List<Card> hand) {
-        return false;
-    }
-
-    private boolean fourOfAKind(List<Card> hand) {
-        return false;
-    }
-
-    private boolean fullHouse(List<Card> hand) {
-        return false;
-    }
-
-    private boolean twoPair(List<Card> hand) {
-        return false;
-    }
-
-    private boolean onePair(List<Card> hand) {
-        return false;
-    }
-
-    private void validateCardList(List<String> cardList) {
-        if(cardList.size() != EXPECTED_CARD_LIST_SIZE){
-            final String message = String.format("Number of elements in cardList: %s does not match expected size: %s", cardList.size(), EXPECTED_CARD_LIST_SIZE);
-            System.out.println(message);
-            throw new PokerFaceException(message);
-        }
-        //TODO Ensure no two values are the same
+        return hand.getHandRank();
     }
 
     private void validateCard(final String card) {
